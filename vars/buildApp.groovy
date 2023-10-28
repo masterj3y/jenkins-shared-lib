bb44ad59d1d95cf28b3678ed4a5f060d1587be3e
MasterJ3y <mjahangiry75@gmail.com>
2023-10-28 16:59:20 +0330

    Fix libraryResource name

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String message) {
-    sh(librayResource('scripts/telnotif.sh $TEL_NOTIFIER_TOKEN $TEL_NOTIF_RECEIVER $message'))
+    sh(libraryResource('scripts/telnotif.sh $TEL_NOTIFIER_TOKEN $TEL_NOTIF_RECEIVER $message'))
     sh 'cargo build --release'
 }

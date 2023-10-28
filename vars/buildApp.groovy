9bb672968e626ac87507d1e5b99a943a6f7d92a8
MasterJ3y <mjahangiry75@gmail.com>
2023-10-28 17:38:41 +0330

    Fix some typo

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String message) {
-    loadBashScripts name: "telnotif.sh"
+    loadBashScript name: "telnotif.sh"
     sh './telnotif.sh $TEL_NOTIFIER_TOKEN $TEL_NOTIF_RECEIVER $message'
     sh 'cargo build --release'
 }

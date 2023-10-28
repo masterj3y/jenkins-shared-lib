84402b73a00bfd22c95c93d4250d92a6d5bac29b
MasterJ3y <mjahangiry75@gmail.com>
2023-10-28 17:55:11 +0330

    Fix some issues

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,6 +2,6 @@
 
 def call(String message) {
     loadBashScript name: "telnotif.sh"
-    sh './telnotif.sh $TEL_NOTIFIER_TOKEN $TEL_NOTIF_RECEIVER ' ${message}
+    sh "./telnotif.sh $TEL_NOTIFIER_TOKEN $TEL_NOTIF_RECEIVER ${message}"
     sh 'cargo build --release'
 }
